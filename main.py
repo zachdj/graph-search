74e63d9b1613efb478cbe6a9973692094c1e4c9d
Zach Jones <zachydj@gmail.com>
2017-09-12 16:56:11 -0400

    Adds radiation problem to main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import searchers.Uninformed as Uninformed, searchers.Informed as Informed
 parser = GraphMLParser()
 
 
-# Romania problem from the textbook:
+####### Romania problem from the textbook #######
 def romania_goal_test(node):
     return node['label'] == 'Bucharest'
 
@@ -45,5 +45,44 @@ romania_problem = Problem(romania_graph, romania_start_node, romania_goal_test)
 romania_sln = Informed.a_star(romania_problem, romania_heuristic)
 # romania_sln = Uninformed.ucs(romania_problem)
 
-print("A* solution for the Romania problem:")
-print(romania_sln)
+print("A* solution for the Romania problem: \n %s \n" % romania_sln)
+
+####### Radiation problem from the HW1 #######
+def radiation_goal_test(node):
+    return node['label'] == '4'
+
+# TODO: come up with a good heuristic for this problem
+radiation_h_table = {
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '5': 0,
+    '6': 0,
+    '7': 0,
+    '8': 0,
+    '9': 0,
+    '10': 0,
+    '11': 0,
+    '12': 0,
+    '13': 0,
+    '14': 0,
+    '15': 0,
+
+}
+def radiation_heuristic(problem, node):
+    return radiation_h_table[ node['label'] ]
+
+radiation_graph = parser.parse("test-graphs/radiation.graphml")
+# At the time of writing, the pygraphml parser is pretty bad and doesn't handle directed/undirected edges very well.
+# so we have to manually make the edges directed here
+for edge in radiation_graph.edges():
+    edge.set_directed(True)
+
+radiation_start_node = "13"
+
+radiation_problem = Problem(radiation_graph, radiation_start_node, radiation_goal_test)
+
+# radiation_sln = Informed.a_star(radiation_problem, radiation_heuristic)
+radiation_sln = Uninformed.ucs(radiation_problem)
+
+print("UCS solution for the Radiation problem: \n %s \n" % radiation_sln)
